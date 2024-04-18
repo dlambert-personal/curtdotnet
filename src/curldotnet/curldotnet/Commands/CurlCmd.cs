768f0db81b7af25d9d1cd93c3024ecce458d3fec
David Lambert <dlambert@appdev.info>
2024-04-17 22:16:45 -0400

    Initial check-in

---
+++ src/curldotnet/curldotnet/Commands/CurlCmd.cs
@@ -0,0 +1,33 @@
+﻿using Spectre.Console.Cli;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace curldotnet.Commands
+{
+    [Description("Curl imitation.")]
+    internal sealed class CurlCmd : Command<CurlCmd.Settings>
+    {
+        public sealed class Settings : CommandSettings
+        {
+            [CommandOption("-s|--startdate [STARTDATE]")]
+            [Description("Begin scanning at this date. Defaults to '[grey]DateTime.Min[/]'.")]
+            public FlagValue<DateTime>? StartDate { get; init; } = new FlagValue<DateTime>() { Value = DateTime.MinValue };
+
+            [CommandOption("-e|--enddate [ENDDATE]")]
+            [Description("Begin scanning at this date. Defaults to '[grey]DateTime.Now[/]'.")]
+            public FlagValue<DateTime>? EndDate { get; init; } = new FlagValue<DateTime>() { Value = DateTime.Now };
+        }
+
+
+        public override int Execute(CommandContext context, Settings settings)
+        {
+            Console.WriteLine("Executing scan-logs");
+
+            return 0;
+        }
+    }
+}
---
+++ src/curldotnet/curldotnet/Program.cs
@@ -0,0 +1,68 @@
+﻿using curldotnet.Commands;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Extensions.Logging;
+using Spectre.Console.Cli;
+
+namespace curldotnet
+{
+    internal class Program
+    {
+        static async Task<int> Main(string[] args)
+        {
+
+            var Configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile(AppDomain.CurrentDomain.BaseDirectory + "\\appsettings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            Log.Logger = new LoggerConfiguration()
+                   .ReadFrom.Configuration(Configuration)
+                   .Enrich.FromLogContext()
+                   .CreateLogger();
+
+            var builder = new HostBuilder()
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddLogging(config =>
+                    {
+                        config.ClearProviders();
+                        config.AddProvider(new SerilogLoggerProvider(Log.Logger));
+                        var minimumLevel = Configuration.GetSection("Serilog:MinimumLevel")?.Value;
+                        if (!string.IsNullOrEmpty(minimumLevel))
+                        {
+                            config.SetMinimumLevel(Enum.Parse<LogLevel>(minimumLevel));
+                        }
+                    });
+                });
+
+            try
+            {
+                //return await builder.RunCommandLineApplicationAsync<HclCmd>(args);
+                var app = new CommandApp();
+                app.Configure(config =>
+                {
+                    config.SetApplicationName("curl-net");
+                    config.ValidateExamples();
+                    config.AddExample("scan-logs", @"--startdate", @"2024-1-1");
+
+                    // Run
+                    config.AddCommand<CurlCmd>("curl");
+                    //config.AddCommand<ScanLogsCmd>("scan-logs");
+
+                });
+                app.SetDefaultCommand<CurlCmd>();
+                return app.Run(args);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return 1;
+            }
+        }
+    }
+}
